a8704a6564bb1cbe4a4bcc16dad9ec5e57edebae
Maverick Seneris <maverickseneris@gmail.com>
2024-05-04 12:51:22 +0800

    refactor

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { useState, useEffect } from "react"
+import Header from "./ui/sections/Header/Header"
+import About from "./ui/sections/About/About"
+import MyStack from "./ui/sections/TechStacks/MyStack"
+import MyProjects from "./ui/sections/Projects/MyProjects"
+import Footer from "./ui/sections/Footer/Footer"
+import './assets/styles/App.css'
+
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(() => JSON.parse(localStorage.getItem('darkMode')) || false)
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  })
+
+  const toggleDarkMode = () => {
+    setDarkMode(isDarkMode => !isDarkMode)
+  }
+
+  const darkTheme = {
+    backgroundColor: darkMode ? "black" : "white",
+    color: darkMode ? "white" : "black",
+    transition: "background 0.7s ease"
+  }
+
+  return (
+    <div style={darkTheme} className="App">
+      <Header
+        toggleDarkMode={toggleDarkMode}
+        darkMode={darkMode}
+      />
+      <About 
+        darkMode={darkMode}
+      />
+      <MyStack 
+        darkMode={darkMode}
+      />
+      <MyProjects />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/projects.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: "Tenzies",
+        image: "https://scrimba.ams3.digitaloceanspaces.com/assets/emails/reactv2/projects/Tenzies.jpg",
+        url: "https://eluids-tenzies-game.netlify.app/",
+        gitHub: "https://github.com/mavSeneris/tenzies",
+        description: "You have ten dice and you must roll as fast as you can until all your dice is on the same number in the shortest time and shout TENZI!",
+        stacks: ["HTML", "CSS", "JavaScript", "React"],
+        type: "Game"
+    },
+
+    {
+        id: 2,
+        name: "E-Commerce UI",
+        image: "https://i.pinimg.com/564x/68/08/5b/68085be21e57a744cfdcca4a6f1e3f31.jpg",
+        url: "https://legendary-youtiao-553c63.netlify.app/",
+        gitHub: "https://github.com/mavSeneris/etape",
+        description: "A cycling apparel online shop that specializes in high performance clothing and other cycling accessories",
+        stacks: ["HTML", "CSS", "JavaScript", "React", "React Router"],
+        type: "E-commerce site"
+    },
+    {
+        id: 3,
+        name: "Fridge2Pan",
+        image: "https://i.im.ge/2023/07/30/9w6uqF.Fridge2App1.png",
+        url: "https://iridescent-centaur-ba6fe9.netlify.app/",
+        gitHub: "https://github.com/mavSeneris/Fridge2Pan",
+        description: "Your ulitimate recipe and meal planning app! Discover recipes, search by available ingredients in your fridge and efficiently plan your meals. Reduce food waste and improve your cooking.",
+        stacks: ["HTML", "CSS", "JavaScript", "React", "React Router", "Firebase", "ChatGPT"],
+        type: "Web app"
+    }
+]
