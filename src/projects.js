d83396eb63e2468c81932fde997013abc6c5afe3
Maverick Seneris <maverickseneris@gmail.com>
2024-05-09 20:35:48 +0800

    feat: add new github links

--- src/projects.js
+++ src/projects.js
@@ -15,7 +15,7 @@ export default [
         name: "E-Commerce UI",
         image: "https://i.pinimg.com/564x/68/08/5b/68085be21e57a744cfdcca4a6f1e3f31.jpg",
         url: "https://maavcc.netlify.app/",
-        gitHub: "https://github.com/mavSeneris/etape",
+        gitHub: "https://github.com/MaverickSeneris/maav",
         description: "A cycling apparel online shop that specializes in high performance clothing and other cycling accessories",
         stacks: ["HTML", "CSS", "JavaScript", "React", "React Router"],
         type: "E-commerce site"
@@ -25,7 +25,7 @@ export default [
         name: "Fridge2Pan",
         image: "https://images.unsplash.com/photo-1606859191214-25806e8e2423?q=80&w=2035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         url: "https://fridge2app.netlify.app/",
-        gitHub: "https://github.com/mavSeneris/Fridge2Pan",
+        gitHub: "https://github.com/MaverickSeneris/Fridge2App",
         description: "Your ulitimate recipe and meal planning app! Discover recipes, search by available ingredients in your fridge and efficiently plan your meals. Reduce food waste and improve your cooking.",
         stacks: ["HTML", "CSS", "JavaScript", "React", "React Router", "Firebase", "ChatGPT"],
         type: "Web app"
