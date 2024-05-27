c0307d9dc4fc3da3764be1a14d293a2e0449a88f
Maverick Seneris <maverickseneris@gmail.com>
2024-05-27 21:28:18 +0800

    feat: add new weather app project

--- src/projects.js
+++ src/projects.js
@@ -1,33 +1,65 @@
 export default [
-    {
-        id: 1,
-        name: "Tenzies",
-        image: "https://scrimba.ams3.digitaloceanspaces.com/assets/emails/reactv2/projects/Tenzies.jpg",
-        url: "https://eluids-tenzies-game.netlify.app/",
-        gitHub: "https://github.com/mavSeneris/tenzies",
-        description: "You have ten dice and you must roll as fast as you can until all your dice is on the same number in the shortest time and shout TENZI!",
-        stacks: ["HTML", "CSS", "JavaScript", "React"],
-        type: "Game"
-    },
+  {
+    id: 1,
+    name: "Tenzies",
+    image:
+      "https://scrimba.ams3.digitaloceanspaces.com/assets/emails/reactv2/projects/Tenzies.jpg",
+    url: "https://eluids-tenzies-game.netlify.app/",
+    gitHub: "https://github.com/mavSeneris/tenzies",
+    description:
+      "You have ten dice and you must roll as fast as you can until all your dice is on the same number in the shortest time and shout TENZI!",
+    stacks: ["HTML", "CSS", "JavaScript", "React"],
+    type: "Game",
+  },
 
-    {
-        id: 2,
-        name: "E-Commerce UI",
-        image: "https://i.pinimg.com/564x/68/08/5b/68085be21e57a744cfdcca4a6f1e3f31.jpg",
-        url: "https://maavcc.netlify.app/",
-        gitHub: "https://github.com/MaverickSeneris/maav",
-        description: "A cycling apparel online shop that specializes in high performance clothing and other cycling accessories",
-        stacks: ["HTML", "CSS", "JavaScript", "React", "React Router"],
-        type: "E-commerce site"
-    },
-    {
-        id: 3,
-        name: "Fridge2Pan",
-        image: "https://images.unsplash.com/photo-1606859191214-25806e8e2423?q=80&w=2035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        url: "https://fridge2app.netlify.app/",
-        gitHub: "https://github.com/MaverickSeneris/Fridge2App",
-        description: "Your ulitimate recipe and meal planning app! Discover recipes, search by available ingredients in your fridge and efficiently plan your meals. Reduce food waste and improve your cooking.",
-        stacks: ["HTML", "CSS", "JavaScript", "React", "React Router", "Firebase", "ChatGPT"],
-        type: "Web app"
-    }
-]
+  {
+    id: 2,
+    name: "E-Commerce UI",
+    image:
+      "https://i.pinimg.com/564x/68/08/5b/68085be21e57a744cfdcca4a6f1e3f31.jpg",
+    url: "https://maavcc.netlify.app/",
+    gitHub: "https://github.com/MaverickSeneris/maav",
+    description:
+      "A cycling apparel online shop that specializes in high performance clothing and other cycling accessories",
+    stacks: ["HTML", "CSS", "JavaScript", "React", "React Router"],
+    type: "E-commerce site",
+  },
+  {
+    id: 3,
+    name: "Fridge2Pan",
+    image:
+      "https://images.unsplash.com/photo-1606859191214-25806e8e2423?q=80&w=2035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://fridge2app.netlify.app/",
+    gitHub: "https://github.com/MaverickSeneris/Fridge2App",
+    description:
+      "Your ulitimate recipe and meal planning app! Discover recipes, search by available ingredients in your fridge and efficiently plan your meals. Reduce food waste and improve your cooking.",
+    stacks: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "React Router",
+      "Firebase",
+      "ChatGPT",
+    ],
+    type: "Web app",
+  },
+  {
+    id: 4,
+    name: "Wther",
+    image:
+      "https://images.unsplash.com/photo-1605727216801-e27ce1d0cc28?w=1800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGh1bmRlcnN0b3JtfGVufDB8fDB8fHww",
+    url: "https://wther.netlify.app/",
+    gitHub: "https://github.com/mavSeneris/Fridge2Pan",
+    description:
+      "Weather App using React, TypeScript, and TailwindCSS, leveraging the Geolocation API to fetch and display the current weather based on the user’s location.",
+    stacks: [
+      "React",
+      "TypeScript",
+      "TailwindCSS",
+      "Openweather API",
+      "Google Geolocation API",
+    ],
+    type: "Web app",
+  },
+];
